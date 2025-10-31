f2b69aef9b103dc34953fad40405f2501a56ccd7
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2025-10-31 09:26:29 +0700

    lanjutin teksedit.kt

--- app/src/main/java/com/example/userinput_20230140162/TeksEdit.kt
+++ app/src/main/java/com/example/userinput_20230140162/TeksEdit.kt
@@ -21,4 +21,24 @@ fun FormDataDiri(modifier: Modifier
     var jenis by remember {mutableStateOf("")}
 
     val gender:list<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+
+            label = { Text(text = "Nama Lengkap") },
+
+            onValueChange = {
+                textName = it
+            }
+        )
+        Row {
+
+    }
 }
